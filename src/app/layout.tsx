bb6d29a3fea9f2bda6d62d651a66843585208ae6
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-05 11:11:19 -0300

    feat: add initial search trip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
-import { NextAuthProvider } from '@/providers/auth'
-import './globals.css'
-import { Poppins } from 'next/font/google'
+import { NextAuthProvider } from "@/providers/auth";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Header from "../components/Header";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "600", "700", "800", "900"] })
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
-  title: 'Vacay Trips',
-  description: 'O seu Sistema de Reservas de Viagens',
-}
+  title: "Vacay Trips",
+  description: "O seu Sistema de Reservas de Viagens",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <Header />
           {children}
         </NextAuthProvider>
       </body>
     </html>
-  )
+  );
 }
