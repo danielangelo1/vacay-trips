924daadd58b1e39befbeda0321c2b57c82456a6f
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-06 11:22:43 -0300

    feat: add location  description migration

---
+++ prisma/migrations/20230706135537_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur aspernatur iste, accusantium ducimus eligendi, ab laborum soluta reprehenderit facere odit minima vitae tenetur!';
