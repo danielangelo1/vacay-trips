9ed3a079353b619a6fcd77f635b41ca2af4918ed
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-06 10:20:48 -0300

    feat: add trip reservation component

---
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Button from "@/components/Button";
+import DatePicker from "@/components/DatePicker";
+import Input from "@/components/input";
+import { Trip } from "@prisma/client";
+import React from "react";
+
+interface TripReservationProps {
+  trip: Trip;
+}
+
+const TripReservation = ({ trip }: TripReservationProps) => {
+  return (
+    <div>
+      <div className="flex flex-col px-5">
+        <div className="flex gap-4">
+          <DatePicker
+            placeholderText="Data de Início"
+            onChange={() => {}}
+            className="w-full"
+          />
+          <DatePicker
+            placeholderText="Data de Saída"
+            onChange={() => {}}
+            className="w-full"
+          />
+        </div>
+
+        <Input
+          placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+          className="mt-4"
+        />
+
+        <div className="flex justify-between mt-2.5">
+          <p className="font-medium text-sm text-primaryDarker">Total: </p>
+          <p className="font-medium text-sm text-primaryDarker">R$: 666 </p>
+        </div>
+
+        <Button className="mt-2.5">Reservar agora</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TripReservation;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import React from "react";
 import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
 
 const getTripsDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -19,10 +20,10 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
 
   return (
     <div className="container mx-auto">
-      {
-        <TripHeader trip={trip} />
-        /* reservation */
-      }
+      <TripHeader trip={trip} />
+
+      {/* reservation */}
+      <TripReservation trip={trip} />
     </div>
   );
 };
