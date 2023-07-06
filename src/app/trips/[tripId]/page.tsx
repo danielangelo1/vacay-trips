4a41efec6a0072cc1449ba4d6eba49721c3d116f
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-06 11:23:15 -0300

    feat: add trip location component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,11 +1,13 @@
 import { prisma } from "@/lib/prisma";
+
 import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
 import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
 
-const getTripsDetails = async (tripId: string) => {
+const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
     where: {
       id: tripId,
@@ -16,7 +18,7 @@ const getTripsDetails = async (tripId: string) => {
 };
 
 const TripDetails = async ({ params }: { params: { tripId: string } }) => {
-  const trip = await getTripsDetails(params.tripId);
+  const trip = await getTripDetails(params.tripId);
 
   if (!trip) return null;
 
@@ -28,6 +30,10 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripReservation trip={trip} />
       <TripDescription description={trip.description} />
       <TripHighlights highlights={trip.highlights} />
+      <TripLocation
+        locationDescription={trip.locationDescription}
+        location={trip.location}
+      />
     </div>
   );
 };
