0ca19945912560333efd4264262e993f8e47481b
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-07 09:22:24 -0300

    feat: add validation to guests input

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -57,7 +57,7 @@ const TripReservation = ({
         message: "Data de início já reservada!",
       });
 
-      setError("endDate", {
+      return setError("endDate", {
         type: "manual",
         message: "Data de saída já reservada!",
       });
@@ -69,7 +69,7 @@ const TripReservation = ({
       });
     }
     if (res?.error?.code === "INVALID_END_DATE") {
-      setError("endDate", {
+      return setError("endDate", {
         type: "manual",
         message: "Data inválida!",
       });
@@ -134,11 +134,16 @@ const TripReservation = ({
             value: true,
             message: "Número de hóspedes é obrigatório!",
           },
+          max: {
+            value: maxGuests,
+            message: `Número de hóspedes deve ser menor ou igual a ${maxGuests}`,
+          },
         })}
         placeholder={`Número de hóspedes (max: ${maxGuests})`}
         className="mt-4"
         error={!!errors?.guests}
         errorMessage={errors?.guests?.message}
+        type="number"
       />
 
       <div className="flex justify-between mt-2.5">
