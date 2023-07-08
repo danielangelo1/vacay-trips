a9fb5fdfa765a330895b998ced0b6740288891e5
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-08 08:32:23 -0300

    feat: add user reservation page

---
+++ src/app/my-trips/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Prisma, TripReservation } from "@prisma/client";
+import UserReservationItem from "./components/UserReservationItem";
+
+const MyTrips = () => {
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
+
+  const { status, data } = useSession();
+
+  const router = useRouter();
+
+  const fetchReservations = async () => {
+    const response = await fetch(
+      `http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`
+    );
+
+    const json = await response.json();
+
+    setReservations(json);
+  };
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      return router.push("/");
+    }
+
+    fetchReservations();
+  }, [status]);
+
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-primaryDarker text-xl">
+        Minhas viagens
+      </h1>
+      {reservations.map((reservation) => (
+        <UserReservationItem key={reservation.id} reservation={reservation} />
+      ))}
+    </div>
+  );
+};
+
+export default MyTrips;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,16 @@ import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  variant?: "primary" | "outlined";
+  variant?: "primary" | "outlined" | "danger";
 }
 
 function Button({ className, variant = "primary", ...props }: ButtonProps) {
   const variantClasses = {
     primary: "bg-primary text-white hover:bg-primaryDarker",
-    outlined: "bg-transparent border-2 border-primary text-primary",
+    outlined:
+      "bg-transparent border-2 border-primary text-primary hover:transition-colors hover:bg-primary hover:text-white",
+    danger:
+      "text-red-500 border-red-500 border hover:bg-red-600 hover:text-white bg-transparent",
   };
 
   const _className = twMerge(
