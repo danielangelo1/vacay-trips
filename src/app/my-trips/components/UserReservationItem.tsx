157fb938f8f1473aefc8a2b2ea2ce74e9039dd70
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-08 08:53:57 -0300

    feat: add reservation cancel

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -5,6 +5,8 @@ import ReactCountryFlag from "react-country-flag";
 import { format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import Button from "@/components/Button";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 interface UserReservationItemProps {
   reservation: Prisma.TripReservationGetPayload<{
@@ -13,7 +15,24 @@ interface UserReservationItemProps {
 }
 
 const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
+  const router = useRouter();
+
   const { trip } = reservation;
+
+  const handleDeleteClick = async () => {
+    const res = await fetch(`/api/trips/reservation/${reservation.id}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      return toast.error("Erro ao cancelar reserva!");
+    }
+    toast.success("Reserva cancelada com sucesso!", {
+      position: "bottom-center",
+    });
+    router.refresh();
+  };
+
   return (
     <div>
       {/* CARD */}
@@ -69,7 +88,7 @@ const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
             </p>
           </div>
 
-          <Button variant="danger" className="mt-5">
+          <Button variant="danger" className="mt-5" onClick={handleDeleteClick}>
             Cancelar
           </Button>
         </div>
