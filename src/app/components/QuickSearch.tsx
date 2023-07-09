35a018015e43bf90333a98f3ae79870bc10cb039
Daniel Angelo <danielangelo1234@gmail.com>
2023-07-08 21:24:49 -0300

    style: fix quick search items align

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -13,7 +13,7 @@ const QuickSearch = () => {
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className="flex w-full justify-between mt-5 lg:m-10 lg:justify-center lg:gap-40">
+      <div className="flex w-full justify-between mt-5 lg:mt-10 lg:justify-center gap-40">
         <div className="flex flex-col items-center gap-1">
           <Link
             href={`/trips/search?text=hotel`}
